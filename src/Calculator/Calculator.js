6d0fbbeb0055afc63289ee6e7706d98cb845a552
fedor-resh <87930362+fedor-resh@users.noreply.github.com>
2022-03-18 17:52:26 +0300

    debug and block not enable characters

--- src/Calculator/Calculator.js
+++ src/Calculator/Calculator.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState,useRef} from 'react';
+import React, {useEffect, useState, useRef} from 'react';
 import p_p from "./calcFunctions";
 import s from './Calculator.module.css'
 import NumberInput from './NumberInput/NumberInput'
@@ -10,7 +10,6 @@ const Calculator = () => {
     const [toRadix, setToRadix] = useState(2)
     const [result, setResult] = useState(0)
     const [error, setError] = useState('')
-
     const characters = '0123456789ABCDEF'
     const charactersFrom = characters.substring(0, fromRadix)
     const charactersTo = characters.substring(0, toRadix)
@@ -18,11 +17,21 @@ const Calculator = () => {
 
     const firstInput = useRef(null)
     const secondInput = useRef(null)
-
     useEffect(() => {
-        if (16 >= fromRadix && fromRadix > 1 && 16 >= toRadix && toRadix > 1 &&
-            !(number.toString().includes('(') && !number.toString().includes(')'))) {
+        auto_fit(firstInput.current)
+        auto_fit(secondInput.current)
+    })
+    useEffect(() => {
+        if (fromRadix > 1 && toRadix > 1 &&
+            !(number.toString().includes('(') && !number.toString().includes(')'))&&
+            number.toString().split('').every((el) =>
+                (charactersFrom+'().').includes(el)
+            )) {
+            setError('')
             setResult(p_p(number, fromRadix, toRadix))
+        } else {
+            setError('Error')
+            console.log('!!!!!!!!!!!!!!!!!!!!!!!!')
         }
     }, [number, fromRadix, toRadix])
 
@@ -31,11 +40,11 @@ const Calculator = () => {
         element.style.width = "5px";
         element.style.width = (element.scrollWidth) + "px";
     }
-    function replaceRadix(){
-        secondInput.current.style.width = "5px";
-        firstInput.current.style.width = "5px";
-        firstInput.current.style.width = (secondInput.current.scrollWidth) + "px";
-        secondInput.current.style.width = (firstInput.current.scrollWidth) + "px";
+
+    function replaceRadix() {
+        const z = firstInput.current.style.width
+        firstInput.current.style.width = secondInput.current.style.width
+        secondInput.current.style.width = z
         const x = fromRadix
         setFromRadix(toRadix)
         setToRadix(x)
@@ -43,19 +52,23 @@ const Calculator = () => {
         setNumber(result)
         setResult(y)
     }
+
     function changeFromRadix(el) {
-        if(el.target.value<=16) {
+        if (el.target.value <= 16) {
             setFromRadix(el.target.value)
             auto_fit(el.target)
         }
     }
 
-    function changeToRadix(num) {
-
+    function changeToRadix(el) {
+        if (el.target.value <= 16) {
+            setToRadix(el.target.value)
+            auto_fit(el.target)
+        }
     }
 
     function changeNumber(num) {
-        if (characters.substring(0, fromRadix).includes(num.charAt(num.length))) {
+        if ((charactersFrom + '().').includes(num[num.length - 1]) || num < number) {
             setNumber(num)
         }
     }
@@ -76,15 +89,16 @@ const Calculator = () => {
                                 changeFromRadix(el)}/>
                         <p className={s.characters}>{charactersFrom}</p>
                     </div>
-                    <img onClick={()=>{replaceRadix()}} src={btn_change} alt=""/>
+                    <img onClick={() => {
+                        replaceRadix()
+                    }} src={btn_change} alt=""/>
                     <div className={s.to__wrapper}>
                         <input
                             ref={secondInput}
-                            onInput={el => auto_fit(el.target)}
                             type="text"
                             value={toRadix}
                             onChange={el =>
-                                setToRadix(el.target.value)}/>
+                                changeToRadix(el)}/>
                         <p className={s.characters}>{charactersTo}</p>
                     </div>
                 </div>
@@ -92,12 +106,12 @@ const Calculator = () => {
                 <div className={s.input__wrapper}>
                     <NumberInput
                         number={number}
-                        setNumber={(num) => setNumber(num)}
+                        setNumber={(num) => changeNumber(num)}
                     />
                 </div>
 
                 <div className={s.result__wrapper}>
-                    <p className={error?s.error:''}>{error?error:result}</p>
+                    <p className={error ? s.error : ''}>{error ? error : result}</p>
                 </div>
             </div>
         </div>
